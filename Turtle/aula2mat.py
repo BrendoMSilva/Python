caceb5dbd6ea4dfb76c3fc138eea3d3786f36cc8
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-08-27 16:55:28 -0300

    initial commit in python

---
+++ Turtle/aula2mat.py
@@ -0,0 +1,5 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Quantos anos você tem: "))
+
+nasc = 2021 - idade
+print(f"Bem vindo, {nome}. Você nasceu em {nasc}."  )
---
+++ Turtle/tat_mat.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+t.shape("turtle")
+t.write("Eu sou a tartaruga ninja")
+t.fd(200)
+t.lt(90)
+t.fd(200)
+t.lt(90)
+t.fd(200)
+t.lt(90)
+t.fd(200)
