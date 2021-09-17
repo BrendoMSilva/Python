854b082ebd9a2d5946c0b14d5f4597b957e02855
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-17 14:56:18 -0300

    numeros_binario_if_else_elif

---
+++ aula4/aula4bin.py
@@ -0,0 +1,2 @@
+for i in range((2 ** 6)+1):
+    print(f'{i:10} {i:10b} {i:10o} {i:10x} {chr(i):>10}')
---
+++ aula4/aula4py.py
@@ -0,0 +1,14 @@
+# if -> se
+# elif -> senão, se
+# else -> senão
+
+x = float(input('Digite X: '))
+y = float(input('Digite Y: '))
+
+if x > y :
+    print('X é maoir que Y')
+elif :
+    if x < y :
+        print('X é menor que Y')
+    else :
+        print('X é igaul a Y')
